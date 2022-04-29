3ab044869e3fba62e5bd5df4033816abb9f0aeb7
Dwayne Hunter <dwaynewhunter@msn.com>
2022-04-29 10:22:15 -0400

    dont validate source domain

--- HunterWebServices.EmailService/Models/MessageDetails.cs
+++ HunterWebServices.EmailService/Models/MessageDetails.cs
@@ -1,8 +1,4 @@
 ﻿using HunterWebServices.EmailService.EmailTemplates;
-using Microsoft.AspNetCore.Http;
-using System;
-using System.Collections.Generic;
-
 namespace HunterWebServices.EmailService.Models;
 
 public class MessageDetails
@@ -11,19 +7,4 @@ public class MessageDetails
     public string Name { get; set; }
     public string Email { get; set; }
     public string Message { get; set; }
-
-    public static Dictionary<EmailType, List<string>> corsUrls = new Dictionary<EmailType, List<string>>()
-    {
-        [EmailType.PortfolioContact] = new List<string>()
-        {
-            "https://hunterwebapps.dev/",
-            "http://localhost:8080/",
-        },
-    };
-
-    public bool IsSourceDomainValid(HttpRequest request)
-    {
-        var referrer = new Uri(request.Headers["Referer"].ToString());
-        return corsUrls[this.Type].Contains(referrer.AbsoluteUri);
-    }
 }
