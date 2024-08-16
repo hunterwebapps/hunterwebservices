7cb5b1fe113348aec4124208e7f4c0f80428948d
HunterWebApps <hunter@hunterwebapps.com>
2024-08-16 11:41:48 +0800

    add new function

---
+++ HunterWebServices.EmailService/Barcode.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace HunterWebServices.EmailService;
+
+public class Barcode
+{
+    [FunctionName("Barcode")]
+    public IActionResult Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "GET", Route = "Barcode")] HttpRequest request,
+        ILogger log)
+    {
+        log.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to Azure Functions!");
+    }
+}
