933015f6c9cad24f66f734c572893ea19de70a59
HunterWebApps <hunter@hunterwebapps.com>
2024-08-24 10:13:13 +0800

    generate barcode with free software

--- HunterWebServices.EmailService/Barcode.cs
+++ HunterWebServices.EmailService/Barcode.cs
@@ -3,8 +3,12 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
-using IronBarCode;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
+using System.IO;
 using System.Linq;
+using ZXing;
+using ZXing.Common;
 
 namespace HunterWebServices.EmailService;
 
@@ -23,7 +27,20 @@ public class Barcode
             return new BadRequestObjectResult("Please provide a barcode value with max 48 characters.");
         }
 
-        var barcode = BarcodeWriter.CreateBarcode(barcodeValue, BarcodeWriterEncoding.Code128);
-        return new FileContentResult(barcode.ToPngBinaryData(), "image/png");
+        var writer = new BarcodeWriterPixelData()
+        {
+            Format = BarcodeFormat.CODE_128,
+            Options = new EncodingOptions()
+            {
+                PureBarcode = true,
+                Height = 100,
+                Width = 300,
+            },
+        };
+        var image = writer.WriteAsImageSharp<SixLabors.ImageSharp.PixelFormats.Rgba32>(barcodeValue);
+        using var memoryStream = new MemoryStream();
+        image.SaveAsPng(memoryStream);
+
+        return new FileContentResult(memoryStream.ToArray(), "image/png");
     }
 }
